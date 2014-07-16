98f1312f1c5b717573f8c4686153ae6681c2dadc
Ryan McCawley <ryan.mccawley@me.com>
2014-07-16 14:33:29 -0400

    Title, Subtitle, Images added

---
+++ Out of this World/OWSpaceObject.h
@@ -0,0 +1,13 @@
+//
+//  OWSpaceObject.h
+//  Out of this World
+//
+//  Created by McCawley on 7/16/14.
+//  Copyright (c) 2014 Anchor-Detail. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OWSpaceObject : NSObject
+
+@end
---
+++ Out of this World/OWSpaceObject.m
@@ -0,0 +1,13 @@
+//
+//  OWSpaceObject.m
+//  Out of this World
+//
+//  Created by McCawley on 7/16/14.
+//  Copyright (c) 2014 Anchor-Detail. All rights reserved.
+//
+
+#import "OWSpaceObject.h"
+
+@implementation OWSpaceObject
+
+@end
